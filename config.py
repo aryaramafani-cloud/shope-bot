b88f45ffefae6657bda991458064b837dcd784a1
Arya <aryaramafani@gmail.com>
2026-05-23 15:21:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TELEGRAM_BOT_TOKEN = "ISI_TOKEN_BOT_KAMU_DISINI"
+TELEGRAM_BOT_TOKEN = "8882245255:AAFjFc9Vj0mEGfBjTECrg0emI1xP0LxxHx0"
 COOKIE_TEMP_PATH = "session_temp.json"   # penyimpanan sementara cookie
